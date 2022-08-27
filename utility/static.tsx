587bf50c3a6420e671cd8fb6c724cb5fc2d44591
LeviChen559 <j89311025@gmail.com>
2022-08-27 00:24:53 -0700

    comment out static.tsx

--- utility/static.tsx
+++ utility/static.tsx
@@ -1,38 +1,46 @@
-import { Character, getCharacterResults } from "../../utility/types"
-import Image from "next/image"
-import imageLoader from "../../utility/imageLoader"
+// import { Character, getCharacterResults } from "./types"
+// import Image from "next/image"
+// import imageLoader from "./imageLoader"
 
 
-const characterPage=({character}:{character:Character})=>{
+// const characterPage=({character}:{character:Character})=>{
    
-    return<>
-        <h2>{character.name}</h2>
-        <Image loader={imageLoader} alt={character.name} src={character.image} unoptimized  width="200" height="200"/>
-    </>
+//     return<>
+//         <h2>{character.name}</h2>
+//         <Image loader={imageLoader} alt={character.name} src={character.image} unoptimized  width="200" height="200"/>
+//     </>
+// }
+
+// export async function getStaticPaths() {
+
+//     const res= await fetch("https://rickandmortyapi.com/api/character")
+//     const {results}:getCharacterResults=await res.json()
+//     return {
+//         paths:results.map((character)=>{
+//             return{
+//                 params:{id: String(character.id)}
+//             }
+//         }),
+//         fallback : false,
+//     }
+// }
+
+// export async function getStaticProps({params}:{params:{id:string}}){
+//     const res =await fetch(
+//         `https://rickandmortyapi.com/api/character/${params.id}`
+//     )
+//     const character = await res.json()
+//     return {
+//         props:{character}
+//     }
+// }
+
+// export default characterPage
+
+import React from 'react'
+
+export default function test() {
+  return (
+    <div>static</div>
+  )
 }
-
-export async function getStaticPaths() {
-
-    const res= await fetch("https://rickandmortyapi.com/api/character")
-    const {results}:getCharacterResults=await res.json()
-    return {
-        paths:results.map((character)=>{
-            return{
-                params:{id: String(character.id)}
-            }
-        }),
-        fallback : false,
-    }
-}
-
-export async function getStaticProps({params}:{params:{id:string}}){
-    const res =await fetch(
-        `https://rickandmortyapi.com/api/character/${params.id}`
-    )
-    const character = await res.json()
-    return {
-        props:{character}
-    }
-}
-
-export default characterPage
